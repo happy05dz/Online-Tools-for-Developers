04f0dd2e1ac694c687b5c39bded0bf0a049c4add
Happy Dzayer <happy05@programmer.net>
2017-04-04 21:06:06 +0200

    Updates

---
+++ tools/html-entities.php
@@ -0,0 +1,111 @@
+<div id="ascii-characters">
+					<h2 style="margin:20px 0 10px 0">ASCII Characters (Printable)</h2>
+					<p>Only printable characters are displayed as control characters (0-31) shouldn't be present in HTML pages since they have no visual representations.</p>
+					<table class="bordered-table zebra-striped" style="font-size:11px;">
+						<thead>
+							<tr>
+								<th nowrap="nowrap">Character</th>
+								<th nowrap="nowrap">Entity Name</th>
+								<th nowrap="nowrap">Entity Number</th>
+								<th nowrap="nowrap">Description</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr><td>&nbsp;</td><td></td><td>&amp;#32;</td><td>Space</td></tr>
+							<tr><td>&#33;</td><td></td><td>&amp;#33;</td><td>Exclamation mark</td></tr>
+							<tr><td>&#34;</td><td></td><td>&amp;#34;</td><td>Quotation mark</td></tr>
+							<tr><td>&#35;</td><td></td><td>&amp;#35;</td><td>Number sign</td></tr>
+							<tr><td>&#36;</td><td></td><td>&amp;#36;</td><td>Dollar sign</td></tr>
+							<tr><td>&#37;</td><td></td><td>&amp;#37;</td><td>Percent sign</td></tr>
+							<tr><td>&#38;</td><td>&amp;amp;</td><td>&amp;#38;</td><td>Ampersand</td></tr>
+							<tr><td>&#39;</td><td></td><td>&amp;#39;</td><td>Apostrophe</td></tr>
+							<tr><td>&#40;</td><td></td><td>&amp;#40;</td><td>Opening/Left Parenthesis</td></tr>
+							<tr><td>&#41;</td><td></td><td>&amp;#41;</td><td>Closing/Right Parenthesis</td></tr>
+							<tr><td>&#42;</td><td></td><td>&amp;#42;</td><td>Asterisk</td></tr>
+							<tr><td>&#43;</td><td></td><td>&amp;#43;</td><td>Plus sign</td></tr>
+							<tr><td>&#44;</td><td></td><td>&amp;#44;</td><td>Comma</td></tr>
+							<tr><td>&#45;</td><td></td><td>&amp;#45;</td><td>Hyphen</td></tr>
+							<tr><td>&#46;</td><td></td><td>&amp;#46;</td><td>Period</td></tr>
+							<tr><td>&#47;</td><td></td><td>&amp;#47;</td><td>Slash</td></tr>
+							<tr><td>&#48;</td><td></td><td>&amp;#48;</td><td>Digit 0</td></tr>
+							<tr><td>&#49;</td><td></td><td>&amp;#49;</td><td>Digit 1</td></tr>
+							<tr><td>&#50;</td><td></td><td>&amp;#50;</td><td>Digit 2</td></tr>
+							<tr><td>&#51;</td><td></td><td>&amp;#51;</td><td>Digit 3</td></tr>
+							<tr><td>&#52;</td><td></td><td>&amp;#52;</td><td>Digit 4</td></tr>
+							<tr><td>&#53;</td><td></td><td>&amp;#53;</td><td>Digit 5</td></tr>
+							<tr><td>&#54;</td><td></td><td>&amp;#54;</td><td>Digit 6</td></tr>
+							<tr><td>&#55;</td><td></td><td>&amp;#55;</td><td>Digit 7</td></tr>
+							<tr><td>&#56;</td><td></td><td>&amp;#56;</td><td>Digit 8</td></tr>
+							<tr><td>&#57;</td><td></td><td>&amp;#57;</td><td>Digit 9</td></tr>
+							<tr><td>&#58;</td><td></td><td>&amp;#58;</td><td>Colon</td></tr>
+							<tr><td>&#59;</td><td></td><td>&amp;#59;</td><td>Semicolon</td></tr>
+							<tr><td>&#60;</td><td>&amp;lt;</td><td>&amp;#60;</td><td>Less-than</td></tr>
+							<tr><td>&#61;</td><td></td><td>&amp;#61;</td><td>Equals sign</td></tr>
+							<tr><td>&#62;</td><td>&amp;gt;</td><td>&amp;#62;</td><td>Greater than</td></tr>
+							<tr><td>&#63;</td><td></td><td>&amp;#63;</td><td>Question mark</td></tr>
+							<tr><td>&#64;</td><td></td><td>&amp;#64;</td><td>At sign</td></tr>
+							<tr><td>&#65;</td><td></td><td>&amp;#65;</td><td>Uppercase A</td></tr>
+							<tr><td>&#66;</td><td></td><td>&amp;#66;</td><td>Uppercase B</td></tr>
+							<tr><td>&#67;</td><td></td><td>&amp;#67;</td><td>Uppercase C</td></tr>
+							<tr><td>&#68;</td><td></td><td>&amp;#68;</td><td>Uppercase D</td></tr>
+							<tr><td>&#69;</td><td></td><td>&amp;#69;</td><td>Uppercase E</td></tr>
+							<tr><td>&#70;</td><td></td><td>&amp;#70;</td><td>Uppercase F</td></tr>
+							<tr><td>&#71;</td><td></td><td>&amp;#71;</td><td>Uppercase G</td></tr>
+							<tr><td>&#72;</td><td></td><td>&amp;#72;</td><td>Uppercase H</td></tr>
+							<tr><td>&#73;</td><td></td><td>&amp;#73;</td><td>Uppercase I</td></tr>
+							<tr><td>&#74;</td><td></td><td>&amp;#74;</td><td>Uppercase J</td></tr>
+							<tr><td>&#75;</td><td></td><td>&amp;#75;</td><td>Uppercase K</td></tr>
+							<tr><td>&#76;</td><td></td><td>&amp;#76;</td><td>Uppercase L</td></tr>
+							<tr><td>&#77;</td><td></td><td>&amp;#77;</td><td>Uppercase M</td></tr>
+							<tr><td>&#78;</td><td></td><td>&amp;#78;</td><td>Uppercase N</td></tr>
+							<tr><td>&#79;</td><td></td><td>&amp;#79;</td><td>Uppercase O</td></tr>
+							<tr><td>&#80;</td><td></td><td>&amp;#80;</td><td>Uppercase P</td></tr>
+							<tr><td>&#81;</td><td></td><td>&amp;#81;</td><td>Uppercase Q</td></tr>
+							<tr><td>&#82;</td><td></td><td>&amp;#82;</td><td>Uppercase R</td></tr>
+							<tr><td>&#83;</td><td></td><td>&amp;#83;</td><td>Uppercase S</td></tr>
+							<tr><td>&#84;</td><td></td><td>&amp;#84;</td><td>Uppercase T</td></tr>
+							<tr><td>&#85;</td><td></td><td>&amp;#85;</td><td>Uppercase U</td></tr>
+							<tr><td>&#86;</td><td></td><td>&amp;#86;</td><td>Uppercase V</td></tr>
+							<tr><td>&#87;</td><td></td><td>&amp;#87;</td><td>Uppercase W</td></tr>
+							<tr><td>&#88;</td><td></td><td>&amp;#88;</td><td>Uppercase X</td></tr>
+							<tr><td>&#89;</td><td></td><td>&amp;#89;</td><td>Uppercase Y</td></tr>
+							<tr><td>&#90;</td><td></td><td>&amp;#90;</td><td>Uppercase Z</td></tr>
+							<tr><td>&#91;</td><td></td><td>&amp;#91;</td><td>Opening/Left square bracket</td></tr>
+							<tr><td>&#92;</td><td></td><td>&amp;#92;</td><td>Backslash</td></tr>
+							<tr><td>&#93;</td><td></td><td>&amp;#93;</td><td>Closing/Right square bracket</td></tr>
+							<tr><td>&#94;</td><td></td><td>&amp;#94;</td><td>Caret</td></tr>
+							<tr><td>&#95;</td><td></td><td>&amp;#95;</td><td>Underscore</td></tr>
+							<tr><td>&#96;</td><td></td><td>&amp;#96;</td><td>Grave accent</td></tr>
+							<tr><td>&#97;</td><td></td><td>&amp;#97;</td><td>Lowercase a</td></tr>
+							<tr><td>&#98;</td><td></td><td>&amp;#98;</td><td>Lowercase b</td></tr>
+							<tr><td>&#99;</td><td></td><td>&amp;#99;</td><td>Lowercase c</td></tr>
+							<tr><td>&#100;</td><td></td><td>&amp;#100;</td><td>Lowercase d</td></tr>
+							<tr><td>&#101;</td><td></td><td>&amp;#101;</td><td>Lowercase e</td></tr>
+							<tr><td>&#102;</td><td></td><td>&amp;#102;</td><td>Lowercase f</td></tr>
+							<tr><td>&#103;</td><td></td><td>&amp;#103;</td><td>Lowercase g</td></tr>
+							<tr><td>&#104;</td><td></td><td>&amp;#104;</td><td>Lowercase h</td></tr>
+							<tr><td>&#105;</td><td></td><td>&amp;#105;</td><td>Lowercase i</td></tr>
+							<tr><td>&#106;</td><td></td><td>&amp;#106;</td><td>Lowercase j</td></tr>
+							<tr><td>&#107;</td><td></td><td>&amp;#107;</td><td>Lowercase k</td></tr>
+							<tr><td>&#108;</td><td></td><td>&amp;#108;</td><td>Lowercase l</td></tr>
+							<tr><td>&#109;</td><td></td><td>&amp;#109;</td><td>Lowercase m</td></tr>
+							<tr><td>&#110;</td><td></td><td>&amp;#110;</td><td>Lowercase n</td></tr>
+							<tr><td>&#111;</td><td></td><td>&amp;#111;</td><td>Lowercase o</td></tr>
+							<tr><td>&#112;</td><td></td><td>&amp;#112;</td><td>Lowercase p</td></tr>
+							<tr><td>&#113;</td><td></td><td>&amp;#113;</td><td>Lowercase q</td></tr>
+							<tr><td>&#114;</td><td></td><td>&amp;#114;</td><td>Lowercase r</td></tr>
+							<tr><td>&#115;</td><td></td><td>&amp;#115;</td><td>Lowercase s</td></tr>
+							<tr><td>&#116;</td><td></td><td>&amp;#116;</td><td>Lowercase t</td></tr>
+							<tr><td>&#117;</td><td></td><td>&amp;#117;</td><td>Lowercase u</td></tr>
+							<tr><td>&#118;</td><td></td><td>&amp;#118;</td><td>Lowercase v</td></tr>
+							<tr><td>&#119;</td><td></td><td>&amp;#119;</td><td>Lowercase w</td></tr>
+							<tr><td>&#120;</td><td></td><td>&amp;#120;</td><td>Lowercase x</td></tr>
+							<tr><td>&#121;</td><td></td><td>&amp;#121;</td><td>Lowercase y</td></tr>
+							<tr><td>&#122;</td><td></td><td>&amp;#122;</td><td>Lowercase z</td></tr>
+							<tr><td>&#123;</td><td></td><td>&amp;#123;</td><td>Opening/Left curly brace</td></tr>
+							<tr><td>&#124;</td><td></td><td>&amp;#124;</td><td>Vertical bar</td></tr>
+							<tr><td>&#125;</td><td></td><td>&amp;#125;</td><td>Closing/Right curly brace</td></tr>
+							<tr><td>&#126;</td><td></td><td>&amp;#126;</td><td>Tilde</td></tr>						 							
+						</tbody>
+					</table>
+				</div>		
